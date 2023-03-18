4d93672b15ea016c0273163aee215a2a305cd0f8
Qasim Abdullah <qasimabdullah404@gmail.com>
2023-03-19 02:01:49 +0500

    add sqlite3 for now

--- __init__.py
+++ __init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = "supersecret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///push_ups_logger.sqlite"
+
+    db.init_app(app)
+
+    from . import models
+    with app.app_context():
+        db.create_all()
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
