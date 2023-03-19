af2bcad6cf1ae93624e30a48a86e78ea56b2f8ff
Qasim Abdullah <qasimabdullah404@gmail.com>
2023-03-20 01:00:32 +0500

    add workout matters

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -7,3 +8,13 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(200))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    workouts = db.relationship('Workout', backref='author', lazy=True)
+
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pushups = db.Column(db.Integer, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    comment = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
